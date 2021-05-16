261032f753bfda6af5dedc45a4ac45a02c0a06e9
Alexandre-ro <alexandre.programador84@gmail.com>
2021-05-16 10:55:33 -0300

    Cadastro de Categorias.

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenda.DAL;
+using SistemaVenda.Entidades;
+using SistemaVenda.Models;
+
+namespace SistemaVenda.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        protected ApplicationDbContext Context;
+
+        public CategoriaController(ApplicationDbContext context) 
+        {
+            this.Context = context;
+        }
+
+        public IActionResult Index()
+        {
+            //Lista com as Categorias
+            IEnumerable<Categoria> lista = Context.Categoria.ToList();
+            Context.Dispose();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id) 
+        {
+            CategoriaViewModel categoria = new CategoriaViewModel();            
+
+            if (id != null) 
+            {
+               var entidade        = Context.Categoria.Where(x => x.codigo == id).FirstOrDefault();
+               categoria.codigo    = entidade.codigo;
+               categoria.descricao = entidade.descricao;
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(CategoriaViewModel entidade) 
+        {
+            if (ModelState.IsValid)
+            {
+                Categoria categoria = new Categoria()
+                {
+                  codigo    = entidade.codigo,
+                  descricao = entidade.descricao
+                };
+
+                if (entidade.codigo == null)
+                {
+                    Context.Categoria.Add(categoria);
+                }
+                else {
+                    Context.Entry(categoria).State = EntityState.Modified;
+                }
+
+                Context.SaveChanges();
+            }
+            else {  
+                return View(entidade);
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id) 
+        {
+            var entidade = new Categoria() { codigo = id };
+            Context.Attach(entidade);
+            Context.Remove(entidade);
+            Context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/CategoriaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Models
+{
+    public class CategoriaViewModel
+    {
+        public int? codigo { get; set; }
+        
+        [Required(ErrorMessage = "O preenchimento da Descrição é obrigatório.")]
+        public string descricao { get; set; }
+    }
+}
